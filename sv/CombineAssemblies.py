5ab2074909afad460458c51e4d84407174e8cfda
Mark Chaisson <mchaisso@uw.edu>
2018-01-16 14:20:06 -0800

    Adding files necessary to run DiploidAnnotation.mak 	new file:   AnnotationPipeline.mak 	new file:   CombineAssemblies.py 	modified:   DiploidAnnotation.mak 	new file:   FilterGapsByTilingPath.py 	new file:   TilingPath.py

---
+++ sv/CombineAssemblies.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import argparse
+import pysam
+
+ap = argparse.ArgumentParser(description="Given a list of assembly sam files, print records into distinct sam files")
+ap.add_argument("--alignments", help="FOFN of alignments.")
+ap.add_argument("--header", help="Header to use.")
+ap.add_argument("--base", help="Base name for haplotypes", default="alignments")
+
+args = ap.parse_args()
+
+headerFile = open(args.header)
+header=headerFile.readlines()
+h0=open(args.base + ".h0.sam", 'w')
+h1=open(args.base + ".h1.sam", 'w')
+h0.write(''.join(header))
+h1.write(''.join(header))
+alignments = open(args.alignments)
+for alnFileName in alignments:
+    alnFile = open(alnFileName.rstrip())
+    
+    for aln in alnFile:
+        vals=aln.split()
+        hap = None
+        for v in vals:
+            if v == "HA:i:1":
+                hap=0
+                h0.write(aln)
+                break                
+            elif v == "HA:i:2":
+                hap=1
+                h1.write(aln)
+                continue
+        if hap is None:
+            sys.stderr.write("ERROR, did not find haplotype for " + vals[0] + "\n")
+            
+
+

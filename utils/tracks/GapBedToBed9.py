084104a937e345196496bdb64443f6664798c336
Mark Chaisson <mchaisso@usc.edu>
2018-02-01 16:10:12 -0800

    Tool for viewing gap bed in browser 	new file:   GapBedToBed9.py

---
+++ utils/tracks/GapBedToBed9.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+inFile = open(sys.argv[1])
+outFile= open(sys.argv[2],'w')
+
+inFile.readline()
+for line in inFile:
+    vals = line.split()
+    outLine=[""]*9
+    outLine[0:3] = vals[0:3]
+    outLine[3] = str(vals[4])
+    outLine[4] = "1000"
+    outLine[5] = "+"
+    outLine[6] = vals[1]
+    outLine[7] = vals[2]
+    if vals[3] == "insertion":
+        outLine[8] = "255,0,0"
+    else:
+        outLine[8] = "0,0,255"
+    outFile.write("\t".join(outLine)+  "\n")
+
